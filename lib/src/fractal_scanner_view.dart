7ec57542b78a3812e8fdd84e1d3ea251abffc150
Mansur5835 <mansurgulomdota5835@gmail.com>
2022-09-04 16:27:20 +0500

    bug  fixed

--- lib/src/fractal_scanner_view.dart
+++ lib/src/fractal_scanner_view.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-import 'dart:isolate';
-
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:fractal_card_scanner/src/scan_logic.dart';
 import 'package:fractal_card_scanner/src/slide_animation.dart';
 
-import 'assets_meneger.dart';
 import 'card_model.dart';
 import 'heart_animation.dart';
 
@@ -82,147 +78,142 @@ class _FractalScannerCardViewState extends State<FractalScannerCardView>
   @override
   Widget build(BuildContext context) {
     if (cameraController?.value.isInitialized ?? false) {
-      return Container(
-        alignment: Alignment.topCenter,
-        child: Container(
-          color: Colors.black,
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          alignment: Alignment.center,
-          child: (cameraController == null)
-              ? Container()
-              : CameraPreview(
-                  cameraController!,
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(left: 10),
-                        height: 60,
-                        alignment: Alignment.centerLeft,
-                        child: FractalHeartAnimation(
-                          animationChild: (size) => Text(
-                            widget.title ?? "Kartani To`g`ri tuting",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: size,
+      return Scaffold(
+        body: Container(
+          alignment: Alignment.topCenter,
+          child: Container(
+            color: Colors.black,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            alignment: Alignment.center,
+            child: (cameraController == null)
+                ? Container()
+                : CameraPreview(
+                    cameraController!,
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(left: 10),
+                          height: 60,
+                          alignment: Alignment.centerLeft,
+                          child: FractalHeartAnimation(
+                            animationChild: (size) => Text(
+                              widget.title ?? "Kartani To`g`ri tuting",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: size,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Container(
-                        alignment: Alignment.topCenter,
-                        child: Container(
-                            alignment: Alignment.bottomRight,
+                        Container(
+                          alignment: Alignment.topCenter,
+                          child: Container(
+                              alignment: Alignment.bottomRight,
+                              width: MediaQuery.of(context).size.width - 10,
+                              height: MediaQuery.of(context).size.height * 0.3,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  border: Border.all(
+                                      width: 3,
+                                      color: widget.primaryColor ??
+                                          const Color(0xff000abc))),
+                              child: widget.appIcon ??
+                                  const FlutterLogo(
+                                    size: 60,
+                                  )),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Container(
                             width: MediaQuery.of(context).size.width - 10,
-                            height: MediaQuery.of(context).size.height * 0.3,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                border: Border.all(
-                                    width: 3,
-                                    color: widget.primaryColor ??
-                                        const Color(0xff000abc))),
-                            child: widget.appIcon ??
-                                const FlutterLogo(
-                                  size: 60,
-                                )),
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      Container(
-                          width: MediaQuery.of(context).size.width - 10,
-                          height: MediaQuery.of(context).size.height * 0.25,
-                          alignment: Alignment.bottomCenter,
-                          child: Stack(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(20),
-                                child: Opacity(
-                                  opacity: 0.9,
-                                  child: SizedBox(
-                                      width: MediaQuery.of(context).size.width -
-                                          10,
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.25,
-                                      child: widget.cardImage),
-                                ),
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.all(10),
-                                    child: Text(
-                                      cardNumber,
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.normal,
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    margin: const EdgeInsets.all(10),
-                                    child: Text(
-                                      cardDate,
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.normal,
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    margin: const EdgeInsets.all(10),
-                                    child: Text(
-                                      name,
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.normal,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Container(
-                                margin: const EdgeInsets.all(15),
-                                alignment: Alignment.topRight,
-                                child: Text(
-                                  bankName,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.normal,
+                            height: MediaQuery.of(context).size.height * 0.25,
+                            alignment: Alignment.bottomCenter,
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Opacity(
+                                    opacity: 0.9,
+                                    child: SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width -
+                                                10,
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.25,
+                                        child: widget.cardImage),
                                   ),
                                 ),
-                              )
-                            ],
-                          )),
-                      const Spacer(),
-                      slideAnimbationController.buildWidget(
-                          child: (controller) => Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  if (scanError) ...{
-                                    Text(
-                                      widget.notFoundCardLabel ??
-                                          "Karta malumotlari topilmadi",
-                                      style: const TextStyle(color: Colors.red),
-                                    )
-                                  },
-                                  Container(
-                                    width: 60,
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                        color: widget.primaryColor ??
-                                            const Color(0xff000abc),
-                                        borderRadius:
-                                            BorderRadius.circular(12312321)),
-                                    child: IconButton(
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      margin: const EdgeInsets.all(10),
+                                      child: Text(
+                                        cardNumber,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.all(10),
+                                      child: Text(
+                                        cardDate,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.all(10),
+                                      child: Text(
+                                        name,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.all(15),
+                                  alignment: Alignment.topRight,
+                                  child: Text(
+                                    bankName,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )),
+                        const Spacer(),
+                        slideAnimbationController.buildWidget(
+                            child: (controller) => Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    if (scanError) ...{
+                                      Text(
+                                        widget.notFoundCardLabel ??
+                                            "Karta malumotlari topilmadi",
+                                        style:
+                                            const TextStyle(color: Colors.red),
+                                      )
+                                    },
+                                    IconButton(
                                       padding: EdgeInsets.zero,
                                       splashRadius: 1,
                                       color: Colors.white,
@@ -237,15 +228,7 @@ class _FractalScannerCardViewState extends State<FractalScannerCardView>
                                         child: widget.scannerIcon,
                                       ),
                                     ),
-                                  ),
-                                  Container(
-                                    width: 60,
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius:
-                                            BorderRadius.circular(12312321)),
-                                    child: IconButton(
+                                    IconButton(
                                       padding: EdgeInsets.zero,
                                       splashRadius: 1,
                                       color: widget.primaryColor ??
@@ -269,9 +252,15 @@ class _FractalScannerCardViewState extends State<FractalScannerCardView>
                                         Navigator.pop(context, cardModel);
                                       },
                                       icon: scanError
-                                          ? const Icon(
-                                              Icons.chevron_right,
-                                              size: 35,
+                                          ? CircleAvatar(
+                                              backgroundColor:
+                                                  widget.primaryColor ??
+                                                      const Color(0xff000abc),
+                                              child: const Icon(
+                                                Icons.chevron_right,
+                                                size: 35,
+                                                color: Colors.white,
+                                              ),
                                             )
                                           : SizedBox(
                                               width: 60,
@@ -279,13 +268,13 @@ class _FractalScannerCardViewState extends State<FractalScannerCardView>
                                               child: widget.confirmationIcon,
                                             ),
                                     ),
-                                  ),
-                                ],
-                              )),
-                      const Spacer(),
-                    ],
+                                  ],
+                                )),
+                        const Spacer(),
+                      ],
+                    ),
                   ),
-                ),
+          ),
         ),
       );
     } else {
